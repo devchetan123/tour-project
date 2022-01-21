e0194e8d932ee4da817521d773d98675d854f7b1
chetan024 <profileofchetan123@gmail.com>
2022-01-22 02:58:58 +0530

    bottom to top button

--- tour/home.js
+++ tour/home.js
@@ -180,3 +180,25 @@ function animate() {
 
 
 // section 2 end //
+
+
+// scroll btn 
+
+
+var mybutton = document.getElementById("s2_topBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
